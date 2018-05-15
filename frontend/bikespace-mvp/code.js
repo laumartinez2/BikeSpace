4d4fe85d81c54667ddaf007a05f197b0a7f98e5e
Lau <lamartinez@esdi.edu.es>
2018-05-15 14:49:26 +0200

    prueba kml mapa

--- frontend/bikespace-mvp/code.js
+++ frontend/bikespace-mvp/code.js
@@ -1,14 +1,20 @@
-
-
 function initMap() {
-        var barcelona = {lat: 41.3942753, lng: 2.1496245};
-    
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: barcelona
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: new google.maps.LatLng(41.385216, 2.1808927),
+          zoom: 16,
+          mapTypeId: 'terrain'
         });
-        var marker = new google.maps.Marker({
-          position: barcelona,
+
+    
+        var src = 'trial.kmlgg';
+        var kmlLayer = new google.maps.KmlLayer(src, {
+          suppressInfoWindows: true,
+          preserveViewport: false,
           map: map
+        });
+        kmlLayer.addListener('click', function(event) {
+          var content = event.featureData.infoWindowHtml;
+          var testimonial = document.getElementById('capture');
+          testimonial.innerHTML = content;
         });
       }
