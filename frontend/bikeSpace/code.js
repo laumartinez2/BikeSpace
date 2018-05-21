0b442bff9fef06481cbcb55bb81f50c52e8a7ac1
Laura <lamartinez@esdi.edu.es>
2018-05-21 17:26:06 +0200

    landing page hecha, solo falta contenido

--- frontend/bikeSpace/code.js
+++ frontend/bikeSpace/code.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     // escondemos .navbar al inicio
     $(".navbar").hide();
 
+    $(".close").click(function() {
+        $('.navbar-collapse').collapse('toggle');
+    });
+
+    $('.nav a').click(function() {
+        $('.navbar-collapse').collapse('toggle');
+        var target = this.hash;
+
+        event.preventDefault();
+
+        var navOffset = $('#navbar').height();
+
+        return $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - navOffset
+        }, 300, function() {
+            return window.history.pushState(null, null, target);
+        });
+    });
+    
     // mostramos la .navbar cuando hayamos hecho 80px de scroll
-    $(function () {
-        $(window).scroll(function () {
+    $(function() {
+        $(window).scroll(function() {
             if ($(this).scrollTop() > 80) {
                 $('.navbar').fadeIn();
             } else {
@@ -13,4 +32,7 @@ $(document).ready(function(){
             }
         });
     });
+
+
+
 });
